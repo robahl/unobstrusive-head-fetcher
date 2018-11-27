24d3617271295bd2b6e1f164608c982ebf1902c1
Robert Ahlberg <rob.ahlberg@gmail.com>
2018-11-27 01:29:00 +0100

    Add HeadersViewer  component

---
+++ src/HeadersViewer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './HeadersViewer.scss';
+
+const HeadersViewer = ({ headers }) => {
+  return <div className="HeadersViewer col mt-3 p-2">HeadersViewer</div>;
+};
+
+export default HeadersViewer;
