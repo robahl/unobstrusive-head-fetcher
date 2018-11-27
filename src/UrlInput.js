517cb0c08fd953f74c5c8e5b94f6bfd3262dc4d9
Robert Ahlberg <rob.ahlberg@gmail.com>
2018-11-27 14:24:35 +0100

    Remove global flag from url regexp

--- src/UrlInput.js
+++ src/UrlInput.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './UrlInput.scss';
 
 export default class UrlInput extends Component {
-  static URL_REGEXP = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gi;
+  static URL_REGEXP = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/i;
 
   constructor() {
     super();
@@ -19,7 +19,7 @@ export default class UrlInput extends Component {
 
   submitUrl(e) {
     e.preventDefault();
-
+    // Check for valid URL
     if (UrlInput.URL_REGEXP.test(this.state.url)) {
       this.props.onSubmit(this.state.url);
     }
