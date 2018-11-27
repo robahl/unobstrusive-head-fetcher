5bb1813fe9af6f6e9cbb844bd42d1bfbe163043d
Robert Ahlberg <rob.ahlberg@gmail.com>
2018-11-27 15:44:32 +0100

    HeadersViewer iterates and renders headers key value pair

--- src/HeadersViewer.js
+++ src/HeadersViewer.js
@@ -2,7 +2,20 @@ import React from 'react';
 import './HeadersViewer.scss';
 
 const HeadersViewer = ({ headers }) => {
-  return <div className="HeadersViewer col mt-3 p-2">HeadersViewer</div>;
+  const headersTable = Object.keys(headers).map(key => (
+    <tr key={key}>
+      <td className="key">{key}</td>
+      <td className="value">{headers[key]}</td>
+    </tr>
+  ));
+
+  const emptyMessage = 'No data';
+
+  return (
+    <div className="HeadersViewer col mt-3 p-2">
+      {headersTable.length > 0 ? <table>{headersTable}</table> : emptyMessage}
+    </div>
+  );
 };
 
 export default HeadersViewer;
